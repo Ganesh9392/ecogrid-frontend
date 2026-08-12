97e83aafa9500e173269f5f8d7ae506ad3acb480
Ganesh9392 <ganeshkrishna328@gmail.com>
2026-08-12 20:26:13 +0530

    updated code

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    // baseURL: "http://127.0.0.1:8000/api",
     // baseURL: "http://13.50.243.95/api",
-    // baseURL: "https://ecogrid-api.duckdns.org/api",
+    baseURL: "https://ecogrid-api.duckdns.org/api",
 });
